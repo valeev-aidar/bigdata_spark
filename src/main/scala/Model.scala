f5bb223b3904745a65b203f0048fd887ada3fa52
Dmitry Grigorev <gridmi007@gmail.com>
2018-11-28 16:32:03 +0300

    Preprocessing added

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -1,50 +1,41 @@
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
-import org.apache.spark.ml.Pipeline
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.linalg.{DenseVector, SparseVector, Vector}
-import org.apache.spark.ml.feature._
+import org.apache.spark.ml.feature.{Tokenizer, _}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.sql._
-import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
-import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.types.DoubleType
 
-import scala.collection.JavaConversions._
-import scala.collection.mutable.ListBuffer
+import TextProcess.process
 
 object Model {
-  case class Twitt(label: Int, sentence: String)
-
-
   def main(args: Array[String]) {
-
-    val col1 = "_1"
-    val col2 = "_2"
 
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
 
     val spark = SparkSession
       .builder
-      .appName("example")
+      .appName("Model")
       .getOrCreate()
 
 
+    var df = spark.read.format("csv")
+      .option("header", "true")
+      //.load("file:///Users/gridm/Desktop/spark-recommendation/spark-recommendation/train.csv")
+      .load("hdfs:///Sentiment/twitter/train.csv")
+      .select("Sentiment", "SentimentText")
+      .toDF("labelStr", "rawText")
 
-    //val df = spark.read.format("csv").option("header", "true").load("csvfile.csv")
-    var df = spark.createDataFrame(Seq(
-      (0.0, "Hi I heard about Spark"),
-      (1.0, "I wish Java could use case classes"),
-      (1.0, "Logistic regression models are neat")
-    )).toDF("label", "text")
+    val clean = udf(process _)
 
+    df = df.withColumn("text", clean(df("rawText")))
+    df = df.withColumn("label", df("labelStr").cast(DoubleType))
 
-    var test = spark.createDataFrame(Seq(
-      (0.0, "I heard about Spark"),
-      (1.0, "I wish Java")
-    )).toDF("label", "text")
+    //val Array(train, test) = df.randomSplit(Array[Double](0.7, 0.3))
+    val train = df
 
-    val tokenizer = new Tokenizer() // standard method in Spark ML
+    val tokenizer = new Tokenizer()
       .setInputCol("text")
       .setOutputCol("words")
     val vectorizer = new CountVectorizer()
@@ -54,123 +45,45 @@ object Model {
       .setInputCol("rawFeatures")
       .setOutputCol("features")
     val lr = new LogisticRegression()
-      .setMaxIter(10)
-      .setRegParam(0.001)
+      .setMaxIter(1000)
+      .setRegParam(1)
     val pipeline = new Pipeline()
       .setStages(Array(tokenizer, vectorizer, idf, lr))
 
-    val model = pipeline.fit(df)
-    // Make predictions on test documents.
-    val res = model.transform(test)
+    val model = pipeline.fit(train)
 
-    res.select("words", "probability", "prediction").show()
-    //println(res)
+    model.write.overwrite().save("/user/edinburgh/model")
 
 
     /*
-        import spark.implicits._
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(lr.regParam, Array(0.1, 1, 10))
+      .build()
 
-        val typedRDD = clean(df).map{case Row(label: Double, sentence: String) => (label, sentence)}
-        val cleanedDF = spark.createDataFrame(typedRDD)
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(new BinaryClassificationEvaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(3)
 
-        val tokenizer = new Tokenizer().setInputCol(col2).setOutputCol("words")
-        val wordsData = tokenizer.transform(cleanedDF)
-
-        val vectorizer = new CountVectorizer()
-          .setInputCol("words").setOutputCol("rawFeatures")
-        val model = vectorizer.fit(wordsData)
-        val featurizedData = model.transform(wordsData)
-
-
-        val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-        val idfModel = idf.fit(featurizedData)
-
-        val rescaledData = idfModel.transform(featurizedData)
-        val trainFeatures = features(rescaledData)
-
-        //var input = asScalaBuffer(trainFeatures.map(row => row.getAs[SparseVector](0))).toDF("features")
-
-        var input = trainFeatures.map(row => row.getAs[SparseVector](0)).toDF("label")
-
-
-        input = mergeDF(input, df)
-
-
-        val convertedVecDF = MLUtils.convertVectorColumnsToML(input)
-
-        val lr = new LogisticRegression()
-          .setMaxIter(100)
-          .setRegParam(0.3)
-
-        // Fit the model
-        val lrModel = lr.fit(convertedVecDF)
-
-        //Testing
-        var test = spark.createDataFrame(Seq(
-          (0.0, "WTF I heard???"),
-          (1.0, "I wish Java")
-        )).toDF(col1, col2)
-
-        val newRDD = clean(test).map{case Row(label: Double, sentence: String) => (label, sentence)}
-        val newDF = spark.createDataFrame(newRDD)
-
-        val wordsTest = tokenizer.transform(newDF)
-        val testData = model.transform(wordsTest)
-
-        val newData = idfModel.transform(testData)
-        val testFeatures = features(newData)
-
-        var testInput = asScalaBuffer(testFeatures.map(row => row.getAs[Array[Double]](0))).toDF("features")
-
-        testInput = mergeDF(testInput, test)
-
-
-        println(lrModel.coefficientMatrix)
+    val model = cv.fit(train)
+    model.avgMetrics.foreach(println)
     */
-    //rdd.map(_.toString()).saveAsTextFile("/user/team3/vec")
-    //rescaledData.select("label", "features").show()
+    val sameModel = PipelineModel.load("/user/edinburgh/model")
+
+    val res = sameModel.transform(train)
+
+    val predictions = res.select("prediction").rdd.map(_.getDouble(0))
+    val labels = res.select("label").rdd.map(_.getDouble(0))
+
+    val accuracy = new MulticlassMetrics(predictions.zip(labels)).accuracy
+
+    print(accuracy)
+
+    //res.select("words", "probability", "prediction").show()
+
+
     spark.stop()
 
   }
-
-  def clean(df: DataFrame): RDD[Row] = {
-    df.rdd.map(row => {
-      val text = row.getAs[String](1)
-      val make = text.replaceAll("[^A-Za-z0-9 ]", "")
-      Row(row(0),make)
-    })
-  }
-
-  def features(rescaledData: org.apache.spark.sql.Dataset[Row]): ListBuffer[Row] = {
-    val list = rescaledData.select("rawFeatures","features").collectAsList().toList
-
-    val features = ListBuffer[Row]()
-
-    for (row <- list) {
-      val tf = row.getAs[SparseVector](0).toArray
-      val idf = row.getAs[SparseVector](1).toArray
-
-      val arr = new Array[Double](tf.length)
-
-      for (i <- tf.indices )
-        arr(i) = tf(i) * idf(i)
-
-      val vec = new SparseVector(idf.length, idf.indices.toArray, arr)
-
-      features.append(Row(vec))
-    }
-
-    features
-  }
-
-  def mergeDF(df1: DataFrame, df2: DataFrame): DataFrame = {
-    val in1 = df1.withColumn("rowId1", monotonically_increasing_id())
-    val in2 = df2.withColumn("rowId2", monotonically_increasing_id())
-
-    val output = in1.as("df1").join(in2.as("df2"), in1("rowId1") === in2("rowId2"), "inner").
-      select("df2._1", "df1.features")
-
-    output.toDF("label", "features")
-  }
-
 }
